6e8e4e2811fc8feda2848c1b73620527bc161979
Akarsh Gangwar <akarshprog28@gmail.com>
2022-06-07 10:06:57 +0530

    top 10 Artist Section completed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import imgg_2 from "./images/imgg_2.jpeg";
 import img_1 from "./images/img_1.jpeg";
 
 import SubHeading1 from './components/SubHeading1';
+import Singer from "./components/Singer";
 
 
 function App() {
@@ -29,11 +30,37 @@ function App() {
           />
           <Song
             Artwork={img_1}
-            song="Meri Mahbhoba"
+            song="kun faya"
             dateOfRelease="August 23,2019"
-            Artist="Kumar Sanu"
+            Artist="Javed Ali"
           />
         </div>
+        <div id="artistHeading">
+                Top 10 Artists
+            </div>
+
+
+            <div id="artist-Division">
+              
+            <div className="subhead">
+        <div id="artist-caption">Artists</div>
+        <div id="artist-caption">Date Of Birth</div>
+        <div id="artist-caption">Song</div>
+      </div>
+          
+        <Singer
+         name="Kumar Sanu"
+         dob="25 April 1987"
+         songslist="Pachtaoge,Humdard,Naina"
+        
+        />
+        <Singer
+                    name="Udit Narayan"
+                    dob="1 December 1955"
+                    songslist="Maahi Ve,Main Yahaan hoon Yahan,Tip Tip"
+                    />
+
+          </div>
        
       </Route>
      
