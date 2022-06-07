36b8c62e148fcab00084e02ae112fa2c953b147c
Akarsh Gangwar <akarshprog28@gmail.com>
2022-06-07 19:08:43 +0530

    Song Rating features fixed

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,20 +1,77 @@
-import { AiOutlineStar } from "react-icons/ai";
+// import { AiOutlineStar } from "react-icons/ai";
+// function Song(props) {
+//   return (
+//     <div className="topsongs">
+//       <div id="songsname">
+//         <div >
+//           <img class="Song-image" src={props.Artwork}></img>
+//         </div>
+//         <div id="song-control">{props.song}</div>
+//         <div id="song-control">{props.dateOfRelease}</div>
+//         <div id="song-control">{props.Artist}</div>
+//         <div class="starstyle">
+//           <AiOutlineStar />
+//           <AiOutlineStar />
+//           <AiOutlineStar />
+//           <AiOutlineStar />
+//           <AiOutlineStar />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Song;
+
+import { React } from "react";
+import { AiFillStar } from "react-icons/ai";
+import { useState } from "react";
+const colors = {
+  orange: "#FFA500",
+  grey: "#808080",
+};
 function Song(props) {
+  const stars = Array(5).fill(0);
+  const [currentValue, setcurrentValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  console.log(stars);
+  const handleClick = (value) => {
+    setcurrentValue(value);
+  };
+
+  const handleMouseOver = (value) => {
+    setHoverValue(value);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverValue(undefined);
+  };
+
   return (
-    <div className="topsongs">
+    <div>
       <div id="songsname">
-        <div >
+        <div id="imgcontrol">
           <img class="Song-image" src={props.Artwork}></img>
         </div>
         <div id="song-control">{props.song}</div>
         <div id="song-control">{props.dateOfRelease}</div>
         <div id="song-control">{props.Artist}</div>
         <div class="starstyle">
-          <AiOutlineStar />
-          <AiOutlineStar />
-          <AiOutlineStar />
-          <AiOutlineStar />
-          <AiOutlineStar />
+          {stars.map((_, index) => {
+            return (
+              <AiFillStar
+                key={index}
+                color={
+                  (hoverValue || currentValue) > index
+                    ? colors.orange
+                    : colors.grey
+                }
+                onClick={() => handleClick(index + 1)}
+                onMouseOver={() => handleMouseOver(index + 1)}
+                onMouseLeave={handleMouseLeave}
+              />
+            );
+          })}
+         
         </div>
       </div>
     </div>
